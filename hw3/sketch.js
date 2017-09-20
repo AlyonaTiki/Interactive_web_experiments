0d07f3e2407ea9e9367dda1d4a0ad47680e1a808
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2017-09-19 20:05:30 -0400

    add var

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,15 +1,15 @@
 
     function setup() {
 	createCanvas(640, 480);
-  
+
     var base = color(125, 129, 135);
-    var lightParts = color(182, 188, 196);
-    var darkParts = color(214, 0, 57);
+    var tp = 186;
+    var pink = color(214, 0, 57);
     var wh = color(256, 256, 256);
 	background("white");
 	noStroke();
 
-  fill(darkParts);
+  fill(pink);
   ellipse(290, 230, 140, 140);
   fill(wh);
   textSize(130);
@@ -17,14 +17,13 @@
   text("AP", 215, 280, 300);
   fill(wh);
   rect(220, 290, 120, 59);
-  triangle(255, 185, 220, 186, 200, 315);
-  triangle(329, 186, 380, 186, 350, 210);
+  triangle(255, 185, 220, tp, 200, 315);
+  triangle(329, tp, 380, tp, 350, 210);
   triangle(329, 244.5, 380, 244.5, 360, 210);
   textSize(30);
-  fill(darkParts);
+  fill(pink);
   text("ALYONA PERMINOVA", 160, 325, 300);
-  //triangle(100, 100, 100, 260, 310, 100);
-  //triangle(230, 180, 100, 280, 330, 280);
+
     }
 function draw() {
 
