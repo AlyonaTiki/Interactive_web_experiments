352c577244a3c4f6fad643910d74acffd873febe
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2017-10-03 18:27:47 -0400

    colors

--- hw5/sketch.js
+++ hw5/sketch.js
@@ -17,22 +17,22 @@ function draw() {
 
 	if (mouseX > width/2 && mouseY > height/2) {
 		if (point) {
-		background("lightgreen");
+		background(225,224,102);
 	}
 		rect(width/2, height/2, width++, height++);
 	} else if (mouseX < width/2 && mouseY > height/2) {
 		if (point) {
-		background("grey");
+		background(37,31,71);
 	}
 		rect(0, height/2, width/2, height++);
 	} else if (mouseX > width/2 && mouseY < height/2) {
 		if (point) {
-		background("lightpink");
+		background(241,95,92);
 	}
 		rect(width/2, 0, width++, height/2);
 	} else {
 		if (point) {
-		background("lightblue");
+		background(164,14,76);
 	}
 		rect(0, 0, width/2, height/2);
 	}
