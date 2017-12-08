2f0c1121e5b892ee5998188a2901b4cbfc160eae
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2017-12-07 20:17:54 -0500

    final2

--- final_2/sketch.js
+++ final_2/sketch.js
@@ -4,13 +4,13 @@ let backgroundColor;
 let bg, bg1;
 let state = 0;
 let scenes = {
-//    start:{
-//        nextScene: "base",
-//    }
+    start:{
+        nextScene: "base",
+    },
     base: {
-        
         title: "Pick your base",
-        nextScene: "toppings",
+        nextScene: "flavor",
+        cal: 0,
         choices: [
             {
                 img: "img/base1.png",
@@ -25,32 +25,81 @@ let scenes = {
                 y: 130
             },
             {
-                img: "img/base2.png",
+                img: "img/base3.png",
                 cal: 120,
                 x: 515,
                 y: 130
             }
         ]
     },
+    flavor: {
+        title: "Pick your flavor",
+        nextScene: "toppings",
+        cal: 0,
+        choices: [
+            {
+                img: "img/top1.png",
+                cal: 30,
+                x: 65,
+                y: 100
+            },
+            {
+                img: "img/top2.png",
+                cal: 35,
+                x: 290,
+                y: 100
+            },
+            {
+                img: "img/top3.png",
+                cal: 40,
+                x: 515,
+                y: 100
+            }
+        ]
+    },
     toppings: {
-
-    }
+        title: "Pick your toppings",
+        nextScene: "calories",
+        cal: 0,
+        choices: [
+            {
+                img: "img/top1.png",
+                cal: 30,
+                x: 65,
+                y: 10
+            },
+            {
+                img: "img/top2.png",
+                cal: 35,
+                x: 290,
+                y: 100
+            },
+            {
+                img: "img/top3.png",
+                cal: 40,
+                x: 515,
+                y: 100
+            }
+        ]
+    },
+    
 };
-let currentScene = "base";
+let currentScene = "start";
 //var buttons = [btnStart, btnNext, btnReset, btnRetry];
 let select_sfx;
 
 function preload() {
     font = loadFont('fonts/Nunito-Bold.ttf');
-    // select_sfx = loadSound("sfx/select3.wav");
+    select_sfx = loadSound("sfx/select3.wav");
     bg = loadImage("img/bg.png");
     bg1 = loadImage("img/bg1.png");
+    ind = loadImage("img/ind.png")
 
     for (var s in scenes) {
         let scene = scenes[s];
         for (var c in scene.choices) {
             let choice = scene.choices[c];
-            choice.button = new cItems(choice.img, choice.x, choice.y, choice.cal);
+            choice.button = new cItems(choice.img, choice.x, choice.y, choice.cal, s);
 
         }
     }
@@ -106,10 +155,9 @@ function cButtons(tImg, tName, tPosX, tPosY) {
 }
 
 ////items object
-function cItems(tImg, tName, tPosX, tPosY, tCal) {
+function cItems(tImg, tPosX, tPosY, tCal, s) {
     //Properties
     this.img = loadImage(tImg);
-    this.name = tName;
     this.PosX = tPosX;
     this.PosY = tPosY;
     this.cal = tCal;
@@ -123,26 +171,30 @@ function cItems(tImg, tName, tPosX, tPosY, tCal) {
         select_sfx.play();
         //Calculate clicable position
         if (mouseX >= this.PosX && mouseX <= this.PosX + 244 && mouseY >= this.PosY && mouseY <= this.PosY + 244) {
-            calories += this.cal;
-
+            // calories += this.cal;
+            scenes[s].cal = this.cal;
         }
     }
 }
 
-function mousePressed() {
+function mouseReleased() {
     //    for (var i = 0; i < buttons.length; i++) {
     //        console.log(buttons[i])
     //        buttons[i].clicked();
     //    }
+   
+    if (scenes[currentScene].choices ) {
+        scenes[currentScene].choices.forEach(function (choice) {
+            choice.button.clicked();
+        });
+    }
+    
+    
     btnStart.clicked();
     btnNext.clicked();
     btnBack.clicked();
     btnReset.clicked();
     btnRetry.clicked();
-    
-    scenes[currentScene].choices.forEach(function (choice) {
-        choice.button.click();
-    });
 
 }
 
@@ -151,13 +203,13 @@ function start() {
     btnStart.display();
     textFont(font, 20);
     fill(0);
-    text("calories = " + calories, 30, 90);
 }
 
 function draw() {
     if (state == 0) {
         start(); //start
     } else if (state == 1) {
+        if (currentScene != "base") currentScene = "base";
         menu("base"); // menu_base(); //base
     } else if (state == 2) {
         menu("flavor"); //menu_flavor(); //flavor
@@ -181,8 +233,9 @@ function menu(scene) {
     fill(255);
     textFont(font, 20);
     fill(0);
+    image(ind, 65, 40);
     text(scenes[scene].title, 320, 38);
-    text("calories = " + calories, 30, 90);
+    text("calories = " + (scenes["base"].cal + scenes["flavor"].cal + scenes["toppings"].cal), 30, 90);
     btnBack.display();
     btnNext.display();
     btnReset.display();
@@ -191,63 +244,68 @@ function menu(scene) {
     });
 }
 
-function menu_flavor() {
-    background(bg);
-    fill(0);
-    textFont(font, 20);
-    text("PICK YOUR FLAVOR", 320, 38);
-    text("calories = " + calories, 30, 90);
-    btnBack.display();
-    btnNext.display();
-    btnReset.display();
-}
+//function menu(scene) {
+//    background(bg);
+//    fill(0);
+//    textFont(font, 20);
+//    //text(scenes[scene].title, 320, 38);
+//    text("calories = " + (scenes["base"].cal + scenes["toppings"].cal), 30, 90);
+//    btnBack.display();
+//    btnNext.display();
+//    btnReset.display();
+//    scenes[scene].choices.forEach(function (choice) {
+//        choice.button.display();
+//});}
+//            
+//function menu(scene) {
+//    background(bg);
+//    fill(255);
+//    textSize(24);
+//    textFont("sans-serif");
+//    text(scenes[scene].title, 320, 38);
+//    text("calories = " + (scenes["base"].cal + scenes["toppings"].cal), 30, 90);
+//    
+//    btnBack.display();
+//    btnNext.display();
+//    btnReset.display();
+//        scenes[scene].choices.forEach(function (choice) {
+//        choice.button.display();
+//});}
 
-function menu_toppings() {
-    background(bg);
-    fill(255);
-    textSize(24);
-    textFont("sans-serif");
-    text("pick your toppings", 30, 50);
-    text("calories = " + calories, 30, 90);
-    btnBack.display();
-    btnNext.display();
-    btnReset.display();
-}
-
-function menu_calories() {
-    background(bg);
-    fill(255);
-    textSize(24);
-    textFont("sans-serif");
-    text("calories", 30, 50);
-    text("calories = " + calories, 30, 90);
-    btnBack.display();
-    btnNext.display();
-    btnReset.display();
-}
-
-function menu_activity() {
-    background(bg);
-    fill(255);
-    textSize(24);
-    textFont("sans-serif");
-    text("pick an activity", 30, 50);
-    text("calories = " + calories, 30, 90);
-    btnBack.display();
-    btnNext.display();
-    btnReset.display();
-}
-
-function menu_result() {
-    background(bg);
-    fill(255);
-    textSize(24);
-    textFont("sans-serif");
-    text("result", 30, 50);
-    btnBack.display();
-    btnNext.display();
-    btnReset.display();
-}
+//function menu_calories() {
+//    background(bg);
+//    fill(255);
+//    textSize(24);
+//    textFont("sans-serif");
+//    text("calories", 30, 50);
+//    text("calories = " + calories, 30, 90);
+//    btnBack.display();
+//    btnNext.display();
+//    btnReset.display();
+//}
+//
+//function menu_activity() {
+//    background(bg);
+//    fill(255);
+//    textSize(24);
+//    textFont("sans-serif");
+//    text("pick an activity", 30, 50);
+//    text("calories = " + calories, 30, 90);
+//    btnBack.display();
+//    btnNext.display();
+//    btnReset.display();
+//}
+//
+//function menu_result() {
+//    background(bg);
+//    fill(255);
+//    textSize(24);
+//    textFont("sans-serif");
+//    text("result", 30, 50);
+//    btnBack.display();
+//    btnNext.display();
+//    btnReset.display();
+//}
 
 function retry() {
     background(backgroundColor);
